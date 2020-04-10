b20bbb2bb8d22e04e3c026e6a63f24c57683a7f6
bewa87 <63112064+bewa87@users.noreply.github.com>
2020-04-10 05:24:16 +0200

    Add files via upload
    GNU Octave - Logistic Power Function and Weibull PDF

---
+++ VectorialLogPowerfunction.m
@@ -0,0 +1,20 @@
+### Piecewise Defined Vectorial Power Output Function
+### with Independent Wind Speed Data Variable For Five
+### Parameters Logistic Regression
+
+function pow_log = VectorialLogPowerfunction(v,z)
+  n       = length(v);
+  pow_log = zeros(n,1);
+  for j = 1:n
+      if     (v(j) >= 0 && v(j) < 2.75)
+        pow_log(j) = 0.0;
+      elseif (v(j) >= 2.75 && v(j) <= 12.50)
+        pow_log(j) = z(1)/(z(2)+z(3)*exp(z(4)*v(j)+z(5)));
+      elseif (v(j) > 12.50 && v(j) <= 25.00)
+        pow_log(j) = 3110;
+      elseif (v(j) > 25.00)
+        pow_log(j) = 0.0;
+      endif
+  endfor
+  pow_log = pow_log';
+endfunction
---
+++ WeibullPDF.m
@@ -0,0 +1,6 @@
+# Vectorial function to calculate probabilities 
+# from Weibull probability density function
+
+function p = WeibullPDF(v,A,k)
+  p = (k/A)*((v./A).^(k-1.0)).*exp(-(v./A).^k);
+endfunction
